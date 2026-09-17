5ed3b5a62cfd93b2593c8967ca761ddac5ba0b58
Dogmatic Polack <kolowemployment@gmail.com>
2026-09-17 14:53:45 -0400

    Adding creating file

--- src/main.zig
+++ src/main.zig
@@ -14,12 +14,39 @@ pub fn main(init: std.process.Init) !void {
     var stdio: Stdio = try .init(allocator, io, 1042);
     defer stdio.deinit();
     var cmd_center: CommandCenter = .init(allocator, io);
-    
-    try stdio.cls();
     _ = try cmd_center.sudoV();
+    
+    const args = try init.minimal.args.toSlice(allocator);
+    const arg = if(args.len >= 2) args[1] else "";
+
+    const config_exists = try createConfig(io);
+    _ = config_exists;
+
+    if(std.mem.eql(u8, arg, "--c")) {
+        try setupConfig(allocator, &stdio, &cmd_center);
+        return;
+    }
+}
+
+fn createConfig(io: Io) !bool {
+    //var buf = [1024]u8;
+    var config_file = blk: {
+        var cwd = Io.Dir.cwd();
+        const cf_file = cwd.openFile(io, "config.json", .{}) catch |err| switch(err) {
+            error.FileNotFound => break :blk try cwd.createFile(io, "config.json", .{}),
+            else => return err,
+        };
+        break :blk cf_file;
+    };
+    defer config_file.close(io);
+    return true;
+}
+
+fn setupConfig(allocator: Allocator, stdio: *Stdio, cmd_center: *CommandCenter) !void {
+    try stdio.cls();
     try stdio.cls();
 
-    const block_devices = try lsblk(allocator, &cmd_center);
+    const block_devices = try lsblk(allocator, cmd_center);
     try stdio.writeln("Select block device: ");
     for(block_devices, 0..) |dev, i| {
         try stdio.print("{d}: {s}\n", .{i, dev.name});
@@ -39,10 +66,13 @@ pub fn main(init: std.process.Init) !void {
         else break :blk mnt_pt;
     };
     
-    const mkdir, const success, _ = try cmd_center.run(&.{"sudo", "mkdir", "-p", mount_pnt});
+    const mkdir, var success, _ = try cmd_center.run(&.{"sudo", "mkdir", "-p", mount_pnt});
     if(!success) try stdio.errorPrint("{s}\n", .{mkdir.stderr}, 1);
 
     _, _, const code = try cmd_center.run(&.{"findmnt", "-rn", "-S", block_dev_loc, ">/dev/null"});
 
-    if(code == 1) _, _, _ = try cmd_center.run(&.{"sudo", "mount", block_dev_loc, mount_pnt});
+    if(code == 1) {
+        const mnt, success, _ = try cmd_center.run(&.{"sudo", "mount", block_dev_loc, mount_pnt});
+        if(!success) try stdio.errorPrint("{s}\n", .{mnt.stderr}, 1);
+    }
 }
